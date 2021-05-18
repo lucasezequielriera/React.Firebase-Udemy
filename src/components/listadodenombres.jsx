785e1a1058153544280097186ac94d0f2e1b7648
Lucas Riera <lucasezequielriera@hotmail.com>
2021-05-17 21:14:47 -0300

    Products List

--- src/components/listadodenombres.jsx
+++ src/components/listadodenombres.jsx
@@ -13,6 +13,7 @@ const Listadodenombres = () => {
             tituloNombre:nombre
         }
         setlistanombres([...listanombres, nuevonombre]);
+        setNombre('');
     }
 
     return(
@@ -21,11 +22,24 @@ const Listadodenombres = () => {
             <div className="row">
                 <div className="col">
                     <h2>Listado de Nombres</h2>
+                    <ul className="list-group">
+                        {
+                            listanombres.map( item =>
+                                <li key={item.id} className="list-group-item">{item.tituloNombre}</li>
+                            )
+                        }
+                    </ul>
                 </div>
                 <div className="col">
                     <h2>Formulario para añadir nombres</h2>
                     <form onSubmit={addNombre} className="form-group">
-                        <input onChange={ (e) => {setNombre(e.target.value)} } className="form-control" type="text" placeholder="Introduce el nombre"/>
+                        <input onChange={ (e) => {
+                            setNombre(e.target.value)} }
+                            className="form-control"
+                            type="text"
+                            placeholder="Introduce el nombre"
+                            value={nombre}/>
+                            
                         <input className="form-control btn btn-info mt-2 mb-2" type="submit" value="Registrar"/>
                     </form>
                 </div>
