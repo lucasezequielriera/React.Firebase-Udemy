761b0733fa7106683c59aa2622ad834aac8a3cfd
Lucas Riera <lucasezequielriera@hotmail.com>
2021-05-19 20:39:04 -0300

    Installing React-Router-Dom, using Router, Switch, Route and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Temperatura from './components/temperatura';
-import Formulario from './components/formulario';
+// import Temperatura from './components/temperatura';
+// import Formulario from './components/formulario';
 import Listadodenombres from './components/listadodenombres';
 import Bienvenida from './components/bienvenida';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Bienvenida
-        nombre="Lucas"
-        edad="25"/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <Temperatura />
-        <hr></hr>
-        <Formulario />
-      </header>
-      <body>
-        <div className="container">
-          <Listadodenombres />
-        </div>
-      </body>
-    </div>
+    <Router>
+      <Link to="/">Base</Link>
+      <Link to="/inicio">Inicio</Link>
+
+      <Switch>
+
+      <Route exact path="/">
+        <br />Esta es la url base
+          <Bienvenida
+            nombre="Lucas"
+            edad="25"
+          />
+        </Route>
+
+        <Route path="/inicio">
+          <br />Estas en Inicio
+          <div className="container">
+            <Listadodenombres />
+          </div>  
+        </Route>
+
+      </Switch>
+      
+    </Router>
+    // <div className="App">
+    //   <header className="App-header">
+        // <Bienvenida
+        // nombre="Lucas"
+        // edad="25"/>
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <Temperatura />
+    //     <hr></hr>
+    //     <Formulario />
+    //   </header>
+    //   <body>
+        // <div className="container">
+        //   <Listadodenombres />
+        // </div>
+    //   </body>
+    // </div>
   );
 }
 
--- src/components/bienvenida.jsx
+++ src/components/bienvenida.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Bienvenida = (props) => {
+    const [Nombre, setNombre] = useState('Pedro');
+    useEffect ( () => {
+        setTimeout( () => {
+            setNombre('Lucas');
+        }, 2000)
+    })
     const {nombre, edad} = props;
     return (
         <div>
             <h2>Bienvenido {nombre} con {edad} años de edad</h2>
+            <h1>{Nombre}</h1>
         </div>
     )
 }
